db845cce9877e087bff4757bf3d5b6e45e94fc1a
Glitch (cedar-tidal-elf) <none>
2022-10-07 14:16:21 +0000

    🕣🍮 Updated with Glitch

---
+++ examples/actions.js
@@ -0,0 +1,102 @@
+// This example shows how to listen to a button click
+// It uses slash commands and actions
+// Require the Bolt package (github.com/slackapi/bolt)
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+async function getConversations() {
+  try {
+    // Call the conversations.list method using the built-in WebClient
+    const result = await app.client.conversations.list({
+      // The token you used to initialize your app
+      token: "xoxb-your-token",
+    });
+
+    return result.channels;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Listen for a slash command invocation
+app.command("/helloworld", async ({ ack, payload, context }) => {
+  // Acknowledge the command request
+  ack();
+
+  const channels = getConversations();
+  console.log(channels);
+
+  try {
+    app.client;
+    const result = await app.client.chat.postMessage({
+      token: context.botToken,
+      // Channel to send message to
+      channel: payload.channel_id,
+      // Include a button in the message (or whatever blocks you want!)
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Go ahead. Click it.",
+          },
+          accessory: {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Click me!",
+            },
+            action_id: "button_abc",
+          },
+        },
+      ],
+      // Text in the notification
+      text: "Message from Test App",
+    });
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Listen for a button invocation with action_id `button_abc`
+// You must set up a Request URL under Interactive Components on your app configuration page
+app.action("button_abc", async ({ ack, body, context }) => {
+  // Acknowledge the button request
+  ack();
+
+  try {
+    // Update the message
+    const result = await app.client.chat.update({
+      token: context.botToken,
+      // ts of message to update
+      ts: body.message.ts,
+      // Channel of message
+      channel: body.channel.id,
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "*The button was clicked!*",
+          },
+        },
+      ],
+      text: "Message from Test App",
+    });
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log("⚡️ Bolt app is running!");
+})();
